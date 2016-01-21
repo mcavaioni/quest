a945e553fe2d00d585bafbf4dff608cfb4c31996
mcavaioni <michele.cavaioni@gmail.com>
2016-01-21 18:52:37 -0500

    created migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120200110) do
+ActiveRecord::Schema.define(version: 20160121232741) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "type"
+    t.boolean  "admin",      default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end

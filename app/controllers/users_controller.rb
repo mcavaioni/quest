bbb333441e9910d6ee461b82d7798270d4ad8024
mcavaioni <michele.cavaioni@gmail.com>
2016-01-25 13:30:13 -0500

    fixed authorization for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_same_user
-  before_action :require_user
+  before_action :require_same_user, except: [:new, :create]
+  before_action :require_user, except: [:new, :create]
 
   def new
     @user=User.new

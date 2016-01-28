edb0a0bb8967d3d3353ac0d823f0991a7e38b710
mcavaioni <michele.cavaioni@gmail.com>
2016-01-28 16:14:29 -0500

    fixed update for questionnaire form

--- app/assets/javascripts/survey_form.js
+++ app/assets/javascripts/survey_form.js
@@ -1,21 +1,3 @@
-$(document).on('page:load', function(){
- 
-  $('#new_question_btn-js').on('click', function(){
-    
-    var numQuestionFieldsLength = $('.question_field-js').length/4
-    var questTitle = $('.question_field-js').clone().get(0).outerHTML;
-    var questionTitleFieldHtmlStr = $('.question_field-js').clone().get(1).outerHTML;
-    var questContent = $('.question_field-js').clone().get(2).outerHTML;
-    var questionContentFieldHtmlStr = $('.question_field-js').clone().get(3).outerHTML;
-    var totalQuestion = questTitle + " " + questionTitleFieldHtmlStr + " " + questContent + " " + questionContentFieldHtmlStr
-    var newQuestionField = totalQuestion.replace(/\d+/g, numQuestionFieldsLength)
-    var wrapped = $(newQuestionField).wrap('<p>')
-    $('.container-quest').append(wrapped)
-    $('.container-quest').append("<p></p>")
-  })
-});
-
-
 $(function(){
  
   $('#new_question_btn-js').on('click', function(){
@@ -32,3 +14,21 @@ $(function(){
     $('.container-quest').append("<p></p>")
   })
 });
+
+//due to turbolinks I need to add additional event listener for page load:
+$(document).on('page:load', function(){
+ 
+  $('#new_question_btn-js').on('click', function(){
+    
+    var numQuestionFieldsLength = $('.question_field-js').length/4
+    var questTitle = $('.question_field-js').clone().get(0).outerHTML;
+    var questionTitleFieldHtmlStr = $('.question_field-js').clone().get(1).outerHTML;
+    var questContent = $('.question_field-js').clone().get(2).outerHTML;
+    var questionContentFieldHtmlStr = $('.question_field-js').clone().get(3).outerHTML;
+    var totalQuestion = questTitle + " " + questionTitleFieldHtmlStr + " " + questContent + " " + questionContentFieldHtmlStr
+    var newQuestionField = totalQuestion.replace(/\d+/g, numQuestionFieldsLength)
+    var wrapped = $(newQuestionField).wrap('<p>')
+    $('.container-quest').append(wrapped)
+    $('.container-quest').append("<p></p>")
+  })
+})

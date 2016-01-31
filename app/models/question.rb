8540596122f421c5f6bae455de5143902193a771
mcavaioni <michele.cavaioni@gmail.com>
2016-01-31 16:29:32 -0500

    added category list for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,55 @@ class Question < ActiveRecord::Base
   has_many :surveys, through: :survey_questions
   has_many :answers
 
-  validates :title, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
+  validates :title, presence: true
   validates :content, presence: true, uniqueness: true, length: {minimum: 3, maximum: 300}
 
+
+ Titles = ["Organization",
+                    "Ownership",
+                    "Corporate Governance",
+                    "Employees",
+                    "Administration/Operations",
+                    "Third Party Relationships",
+                    "Track Record",
+                    "Risk Management",
+                    "Operational Risk",
+                    "Investment Research",
+                    "Execution & Trading",
+                    "Technology",
+                    "Treasury",
+                    "Compliance",
+                    "Legal",
+                    "Anti-Money Laundering",
+                    "Insurance",
+                    "Business Continuity",
+                    "Fund Details",
+                    "Fees",
+                    "Share Class Terms",
+                    "Fund Directors",
+                    "Service Providers",
+                    "Fund Administrator",
+                    "Fund Pricing",
+                    "Prime Broker",
+                    "Custodian",
+                    "Auditor",
+                    "Legal Advisers",
+                    "General",
+                    "Promoters",
+                    "Fund Assets",
+                    "Capacity Management",
+                    "Withdrawals",
+                    "Management Team Co-Investment",
+                    "Investor Breakdown",
+                    "Fund Performance",
+                    "Investment Strategy",
+                    "Portfolio Construction",
+                    "Leverage",
+                    "Hedging",
+                    "Liquidity",
+                    "Diversification",
+                    "External Controls",
+                    "Investor Reporting",
+                    "Taxation",
+                    "DDQ"]
 end

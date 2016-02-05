f3adab7db16efee4912af308d74f73294421dab4
mcavaioni <michele.cavaioni@gmail.com>
2016-02-05 16:11:17 -0500

    added ajax success response and css color for answers new page

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,25 +1,24 @@
 $(function(){
   $('.answer-btn-js').parent().on ('ajax:success', function(event, data, status, xhr){
-    
-    // document.getElementById("answer_content").value=""
-    // $("#selElement_Id option:selected").removeAttr("selected")
     var template = (data.template);
 debugger;
     $(this).parent().append(template)
-    // this.getElementsByClassName('answer-form')=""
-    // data.template=""
-    // document.getElementById("answer_content").value=""
+//if i want to keep the text underneath don't put the line below
     $('textarea').val('')
+    $(this).parent().addClass('displaying')
+    $(this).parent().parent().addClass('change-color')
+    // $(this).parents('.parent').css( "color", "red" )
+
   })
 })
 
 
-function removeElement(parentDiv, childDiv){
+// function removeElement(parentDiv, childDiv){
           
-          var child = document.getElementById(childDiv);
-          var parent = document.getElementById(parentDiv);
-          parent.removeChild(child);
+//           var child = document.getElementById(childDiv);
+//           var parent = document.getElementById(parentDiv);
+//           parent.removeChild(child);
 
-}
+// }
 
-$('.answer-btn-js').parent().on('click', removeElement())
+// $('.answer-btn-js').parent().on('click', removeElement())

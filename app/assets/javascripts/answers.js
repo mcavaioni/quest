bdf0b89e1d84e8757f897c93c4586a6dcfb562f6
mcavaioni <michele.cavaioni@gmail.com>
2016-02-06 08:22:12 -0500

    added listener for answer to to turbolinks interference

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,24 +1,24 @@
 $(function(){
   $('.answer-btn-js').parent().on ('ajax:success', function(event, data, status, xhr){
     var template = (data.template);
-debugger;
     $(this).parent().append(template)
 //if i want to keep the text underneath don't put the line below
     $('textarea').val('')
     $(this).parent().addClass('displaying')
     $(this).parent().parent().addClass('change-color')
     // $(this).parents('.parent').css( "color", "red" )
-
   })
 })
 
-
-// function removeElement(parentDiv, childDiv){
-          
-//           var child = document.getElementById(childDiv);
-//           var parent = document.getElementById(parentDiv);
-//           parent.removeChild(child);
-
-// }
-
-// $('.answer-btn-js').parent().on('click', removeElement())
+//due to turbolinks I need to add additional event listener for page load:
+$(document).on('page:load', function(){
+  $('.answer-btn-js').parent().on ('ajax:success', function(event, data, status, xhr){
+    var template = (data.template);
+    $(this).parent().append(template)
+//if i want to keep the text underneath don't put the line below
+    $('textarea').val('')
+    $(this).parent().addClass('displaying')
+    $(this).parent().parent().addClass('change-color')
+    // $(this).parents('.parent').css( "color", "red" )
+  })
+})
